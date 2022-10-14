20368fdb056accfde1aaf206f14086d8fdaf246c
Leonardo Paiva Caldas <106037840+leo5123@users.noreply.github.com>
2022-10-14 14:57:02 -0300

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'normalize.css'
+import Cardapio from './pages/Cardapio';
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+  <Cardapio/>
+  </React.StrictMode>
+);
+
+
